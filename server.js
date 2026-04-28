666714d332632e66cfef95e07e63b09cb86ba227
Marshel-S <kace93126@gmail.com>
2026-04-28 15:57:44 +0700

    Deploy

--- server.js
+++ server.js
@@ -793,6 +793,7 @@ app.post("/api/contact", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Server running di http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running in ${PORT}`);
 });
