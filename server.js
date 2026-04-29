6677a92f14303acf761a3d2723d015c22205b768
Marshel-S <kace93126@gmail.com>
2026-04-29 10:52:30 +0700

    Seperated Frontend & Backend

--- server.js
+++ server.js
@@ -1,27 +1,29 @@
 const express = require("express");
+const serverless = require("serverless-http");
+const session = require("express-session");
 const authRoutes = require("./routes/auth");
 const db = require("./db");
 const multer = require("multer");
 const path = require("path")
 const nodemailer = require("nodemailer");
 
+const app = express();
+
+const baseDir = process.cwd();
+
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: "dehausst@gmail.com",
-    pass: "xmll cvqy qkni jqhv"
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
   }
 });
-
-const app = express();
 
 const activeUsers = [];
 
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));;
-
-const session = require("express-session");
 
 app.set('trust proxy', 1);
 
@@ -73,6 +75,9 @@ function isAuthenticated(req, res, next) {
 }
 
 app.use(isAuthenticated);
+
+app.use(express.static(path.join(baseDir, "public")));
+app.use("/uploads", express.static(path.join(baseDir, "uploads")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "login.html"));
@@ -793,7 +798,4 @@ app.post("/api/contact", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running in ${PORT}`);
-});
+module.exports.handler = serverless(app);
